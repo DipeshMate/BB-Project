7b8cdc08e148dee1067d3fa3f43ea3fa852b7530
Dipesh Mate <dipeshmate17@gmail.com>
2025-01-21 10:56:07 +0530

    Build a Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
   return (
     <>
-      <h>BB New Project</h>
+      <div className='bg-slate-900 m-6'>
+
+      <Navbar/>
+      {/* <h1 className='text-teal-700 text-3xl text-center'>BB New Project</h1> */}
+      </div>
     </>
   )
 }
